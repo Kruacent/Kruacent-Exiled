cebee5562d492c960fb8d68f5a4a9f7d157e093e
Patrique29 <baka.coca@gmail.com>
2024-12-08 18:26:10 +0100

    Added empty GE

---
+++ KruacentE.GlobalEventFramework/GEFE.Examples/GE/R.cs
@@ -0,0 +1,24 @@
+﻿using Exiled.API.Features;
+using GEFExiled.GEFE.API.Features;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEFExiled.GEFE.Examples.GE
+{
+    public class R : GlobalEvent
+    {
+        public override int Id { get; set; } = 32;
+        public override string Name { get; set; } = "nothing";
+        public override string Description { get; set; } = "y'a r";
+        public override double Weight { get; set; } = 1;
+        public override IEnumerator<float> Start()
+        {
+            yield return 0;
+        }
+
+    }
+}
