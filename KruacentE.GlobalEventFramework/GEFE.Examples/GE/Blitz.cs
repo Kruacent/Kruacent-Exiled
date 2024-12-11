ac26306d6f932de66a848cd7dff2ab85f3ea74c7
Patrique29 <baka.coca@gmail.com>
2024-12-11 22:59:23 +0100

    Started Blitz Global Event

---
+++ KruacentE.GlobalEventFramework/GEFE.Examples/GE/Blitz.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using GEFExiled.GEFE.API.Features;
+using InventorySystem.Items.ThrowableProjectiles;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KruacentExiled.KruacentE.GlobalEventFramework.GEFE.Examples.GE
+{
+    public class R : GlobalEvent
+    {
+        public override int Id { get; set; } = 1;
+        public override string Name { get; set; } = "Blitz";
+        public override string Description { get; set; } = "éteignez les lumières la luftwaffe arrive";
+        public override double Weight { get; set; } = 1;
+        public int Cooldown { get; set; } = 120;
+        public override IEnumerator<float> Start()
+        {
+            while (!Round.IsEnded)
+            {
+                yield return Timing.WaitForOneFrame;
+                ((ExplosiveGrenade)Item.Create(ItemType.GrenadeHE)).SpawnActive(Room.Random().Position); 
+
+            }
+        }
+
+    }
+}
