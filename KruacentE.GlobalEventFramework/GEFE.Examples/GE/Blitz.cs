d044f5a9a5b6e25b1dac5e3b18aff30ef2587de8
Patrique29 <baka.coca@gmail.com>
2024-12-11 23:41:32 +0100

    Finished blitz

--- KruacentE.GlobalEventFramework/GEFE.Examples/GE/Blitz.cs
+++ KruacentE.GlobalEventFramework/GEFE.Examples/GE/Blitz.cs
@@ -9,21 +9,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KruacentExiled.KruacentE.GlobalEventFramework.GEFE.Examples.GE
+namespace GEFExiled.GEFE.Examples.GE
 {
-    public class R : GlobalEvent
+    public class Blitz : GlobalEvent
     {
         public override int Id { get; set; } = 1;
         public override string Name { get; set; } = "Blitz";
         public override string Description { get; set; } = "éteignez les lumières la luftwaffe arrive";
         public override double Weight { get; set; } = 1;
         public int Cooldown { get; set; } = 120;
+        public int NbGrenadeSpawned { get; set; } = 5;
         public override IEnumerator<float> Start()
         {
             while (!Round.IsEnded)
             {
-                yield return Timing.WaitForOneFrame;
-                ((ExplosiveGrenade)Item.Create(ItemType.GrenadeHE)).SpawnActive(Room.Random().Position); 
+                Log.Debug("waiting");
+                yield return Timing.WaitForSeconds(Cooldown);
+                for (int i = 0; i < NbGrenadeSpawned; i++)
+                {
+                    ((ExplosiveGrenade)Item.Create(ItemType.GrenadeHE)).SpawnActive(Room.Random().Position);
+                }
+                
 
             }
         }
