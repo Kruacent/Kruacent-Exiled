058ab9442357e4b8b73d51cebb1c3f43677413f8
Patrique29 <baka.coca@gmail.com>
2024-12-13 08:56:05 +0100

    switched EndingRound to RoundEnded

--- KruacentE.GlobalEventFramework/GEFE/Handlers/ServerHandler.cs
+++ KruacentE.GlobalEventFramework/GEFE/Handlers/ServerHandler.cs
@@ -23,20 +23,24 @@ namespace GEFExiled.Handlers
 		{
 			Log.Debug("starting round");
             _activeGE = _plugin.ChooseGE();
+            Log.Debug("sub event");
             _activeGE.ForEach(e => e.SubscribeEvent());
-			_plugin.Show();
+            Log.Debug("show to player");
+            _plugin.Show();
             Log.Debug("end starting round");
         }
 
-		public void OnEndingRound(EndingRoundEventArgs _)
+		public void OnEndingRound(RoundEndedEventArgs _)
 		{
             Log.Debug("ending round");
             _activeGE.ForEach(e => e.UnsubscribeEvent());
+            Timing.KillCoroutines();
         }
         public void OnRestartingRound()
         {
             Log.Debug("restarting");
             _activeGE.ForEach(e => e.UnsubscribeEvent());
+            Timing.KillCoroutines();    
         }
 
     }
