7e37e1431c00847de42195dc9c368333ee23e9de
Patrique29 <baka.coca@gmail.com>
2025-01-12 07:37:01 +0100

    fixed the presse purée and reduce its damage

--- KruacentExiled/KE.Items/Items/PressePuree.cs
+++ KruacentExiled/KE.Items/Items/PressePuree.cs
@@ -8,15 +8,16 @@ using Exiled.CustomItems.API.Features;
 
 namespace KE.Items.Items
 {
+    [CustomItem(ItemType.GrenadeHE)]
     public class PressePuree : CustomGrenade
     {
         public override uint Id { get; set; } = 1406;
         public override string Name { get; set; } = "Presse Purée";
-        public override string Description { get; set; } = "The grenade explose at impact";
+        public override string Description { get; set; } = "The grenade explode at impact but does less damage";
         public override float Weight { get; set; } = 0.65f;
         public override float FuseTime { get; set; } = 1.5f;
         public override bool ExplodeOnCollision { get; set; } = true;
-        public float DamageModifier { get; set; } = 1f;
+        public float DamageModifier { get; set; } = 0.9f;
         public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
         {
             Limit = 5,
