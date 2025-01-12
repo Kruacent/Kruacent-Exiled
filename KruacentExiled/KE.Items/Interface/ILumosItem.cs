f0d8bb703ece7db2eb1c2e1119ad9a5c381e8173
Patrique29 <baka.coca@gmail.com>
2025-01-12 07:36:23 +0100

    added the possibility to have light on custom items

---
+++ KruacentExiled/KE.Items/Interface/ILumosItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KE.Items.Interface
+{
+    internal interface ILumosItem
+    {
+
+        UnityEngine.Color Color { get; set; }
+    }
+}
