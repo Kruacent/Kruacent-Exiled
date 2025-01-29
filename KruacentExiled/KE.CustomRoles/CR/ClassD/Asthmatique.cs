81fa122d51467e1d514cdb0518e7aa92f96c5689
Patrique29 <baka.coca@gmail.com>
2025-01-29 14:41:30 +0100

    corrected yet again the Asthmatique

--- KruacentExiled/KE.CustomRoles/CR/ClassD/Asthmatique.cs
+++ KruacentExiled/KE.CustomRoles/CR/ClassD/Asthmatique.cs
@@ -1,6 +1,7 @@
 ﻿using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
+using Exiled.CustomRoles.API.Features;
 using PlayerRoles;
 using UnityEngine;
 using Utils.NonAllocLINQ;
@@ -8,7 +9,7 @@ using Utils.NonAllocLINQ;
 namespace KE.CustomRoles.CR.ClassD
 {
     [CustomRole(RoleTypeId.ClassD)]
-    internal class Asthmatique : Exiled.CustomRoles.API.Features.CustomRole
+    internal class Asthmatique : CustomRole
     {
         public override string Name { get; set; } = "Asthmatique";
         public override string Description { get; set; } = "Tu es <color=#BFFF00>asthmatique</color>\nT'as stamina est réduit de moitié\nMais tu vises mieux";
@@ -19,10 +20,9 @@ namespace KE.CustomRoles.CR.ClassD
         public override bool KeepRoleOnDeath { get; set; } = true;
         public override bool KeepRoleOnChangingRole { get; set; } = true;
         public override bool IgnoreSpawnSystem { get; set; } = true;
-        public override void AddRole(Player player)
+        protected override void RoleAdded(Player player)
         {
-            player.ShowHint("Tu es <color=#BFFF00>asthmatique</color>\nT'as stamina est réduit de moitié\nMais tu vises mieux");
-            player.EnableEffect(EffectType.Scp1853, -1,true);
+            player.EnableEffect(EffectType.Scp1853, -1, true);
             player.EnableEffect(EffectType.Exhausted, -1, true);
         }
     }
