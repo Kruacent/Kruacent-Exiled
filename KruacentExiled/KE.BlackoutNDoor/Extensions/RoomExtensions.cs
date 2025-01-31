12f46c393a64052647c688e945dc90bd83bcf8de
Patrique29 <baka.coca@gmail.com>
2025-01-31 11:46:30 +0100

    added room extension to check if it's safe or not

---
+++ KruacentExiled/KE.BlackoutNDoor/Extensions/RoomExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Enums;
+using Discord;
+
+namespace KE.BlackoutNDoor.Extensions
+{
+    public static class RoomExtensions
+    {
+        public static bool IsSafe(this Room room)
+        {
+            return IsSafe(room.Zone);
+        }
+
+
+        public static bool IsSafe(this ZoneType zone)
+        {
+            bool result = true;
+            if (zone == ZoneType.LightContainment)
+                result = Map.DecontaminationState < DecontaminationState.Countdown;
+            switch (zone)
+            {
+                case ZoneType.LightContainment:
+                case ZoneType.HeavyContainment:
+                case ZoneType.Entrance:
+                    result = !Warhead.IsDetonated;
+                    break;
+            }
+            return result;
+        }
+    }
+}
