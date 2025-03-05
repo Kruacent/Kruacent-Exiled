9564fd696ba7a952708f3ea476350d35cc59bb5f
Patrique29 <baka.coca@gmail.com>
2025-03-05 03:07:04 +0100

    changed the chance constructor of upgrade properties

--- KruacentExiled/KE.Items/Upgrade/UpgradeProperties.cs
+++ KruacentExiled/KE.Items/Upgrade/UpgradeProperties.cs
@@ -5,35 +5,30 @@ using System.Linq;
 using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.Core.Tokens;
 
 namespace KE.Items.Upgrade
 {
     public class UpgradeProperties
     {
-        private float _chance = 0;
+        private float _chance;
         public float Chance
         {
             get { return _chance; }
-            set
-            {
-                if (_chance > 100)
-                    _chance = 100;
-                else
-                    _chance = value;
-            }
         }
 
         private uint _newItem;
-
         public uint UpgradedItem
         {
             get { return _newItem; }
         }
         
-        public UpgradeProperties(byte chance, uint newItem)
+        public UpgradeProperties(float chance, uint newItem)
         {
-            Chance = chance;
             _newItem = newItem;
+            if (chance > 100) _chance = 100;
+            else if(chance < 0) _chance = 0;
+            else _chance = chance;
         }
 
     }
