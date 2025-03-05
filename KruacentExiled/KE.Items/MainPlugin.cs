e2d8b6761a4dc30f97c82cec11a1c9350dd08cbe
Patrique29 <baka.coca@gmail.com>
2025-03-05 02:58:53 +0100

    reworked the lights

--- KruacentExiled/KE.Items/MainPlugin.cs
+++ KruacentExiled/KE.Items/MainPlugin.cs
@@ -5,6 +5,8 @@ using Exiled.API.Features.Toys;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Player;
 using KE.Items.Interface;
+using KE.Items.Lights;
+using KE.Items.Upgrade;
 using MEC;
 using System;
 using System.Collections.Generic;
@@ -17,20 +19,23 @@ namespace KE.Items
         public override string Author => "Patrique & OmerGS";
         public override string Name => "KEItems";
         internal Sound Sound { get; private set; }
+        internal UpgradeHandler UpgradeHandler { get; private set; }
+        internal LightsHandler LightsHandler { get; private set; }
         internal static MainPlugin Instance { get; private set; }
-        public float Intensity { get; set; } = .5f;
+        
         public override Version Version => new Version(1, 0, 0);
-        private Dictionary<Pickup, Light> pl = new Dictionary<Pickup, Light>();
+        
         public override void OnEnabled()
         {
             Instance = this;
             Sound = new Sound();
-
+            UpgradeHandler = new UpgradeHandler();
+            LightsHandler = new LightsHandler();
 
             CustomItem.RegisterItems();
-            Exiled.Events.Handlers.Player.DroppedItem += Drop;
-            Exiled.Events.Handlers.Player.PickingUpItem += Pick;
-            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            UpgradeHandler.SubscribeEvents();
+            LightsHandler.SubscribeEvents();
+
             
             base.OnEnabled();
         }
@@ -38,81 +43,15 @@ namespace KE.Items
         public override void OnDisabled()
         {
             CustomItem.UnregisterItems();
-            Exiled.Events.Handlers.Player.DroppedItem -= Drop;
-            Exiled.Events.Handlers.Player.PickingUpItem -= Pick;
-            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            UpgradeHandler.UnsubscribeEvents();
+            LightsHandler.UnsubscribeEvents();
+
 
             base.OnDisabled();
+            LightsHandler = null;
             Sound = null;
+            UpgradeHandler = null;
             Instance = null;
-        }
-
-        public void Pick(PickingUpItemEventArgs ev)
-        {
-            Pickup pickup = ev.Pickup;
-            if (pl.ContainsKey(pickup))
-            {
-                Light val = pl[pickup];
-                if (val != null)
-                {
-                    val.Destroy();
-                }
-                pl.Remove(pickup);
-            }
-        }
-        public void Drop(DroppedItemEventArgs ev)
-        {
-            Pickup pickup = ev.Pickup;
-            if (CustomItem.TryGet(pickup, out CustomItem item) && item is ILumosItem ci)
-            {
-                pl.Add(pickup, null);
-            }
-
-        }
-        public void OnRoundStarted()
-        {
-            Timing.RunCoroutine(LightP());
-        }
-
-        internal IEnumerator<float> LightP()
-        {
-            foreach (var p in Pickup.List)
-            {
-                if (p != null)
-                {
-                    if (CustomItem.TryGet(p, out CustomItem ci) && ci is ILumosItem)
-                        pl.Add(p, null);
-                }
-
-            }
-
-            while (Round.InProgress)
-            {
-                
-                foreach (var x in pl.ToList())
-                {
-                    if(CustomItem.TryGet(x.Key, out CustomItem cui) && cui is ILumosItem ci)
-                    {
-                        Light light = Light.Create(x.Key.Position, null, null, true, ci.Color);
-                        light.Intensity = Intensity;
-                        if (x.Value != null)
-                        {
-                            Light val = x.Value;
-                            val.Destroy();
-                        }
-                        pl[x.Key] = light;
-                        //Log.Debug(x.Key.Position+";"+x.Value.Position);
-                    }
-                    else
-                    {
-                        Light val = x.Value;
-                        val.Destroy();
-                        pl.Remove(x.Key);
-                    }
-                }
-                yield return Timing.WaitForSeconds(Instance.Config.RefreshRate);
-            }
-
         }
 
     }
