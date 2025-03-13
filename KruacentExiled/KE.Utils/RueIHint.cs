19c51cfdfdf789c78135f4faac2f404a684a1d64
Patrique29 <baka.coca@gmail.com>
2025-03-13 08:48:11 +0100

    added utils from global event

---
+++ KruacentExiled/KE.Utils/RueIHint.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.Commands.PluginManager;
+using RueI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KE.Utils
+{
+
+    /// <summary>
+    /// Hint with position
+    /// </summary>
+    public class RueIHint
+    {
+        public Hint Hint { get; set; }
+        public float Position { get; set; }
+        public RueIHint(Hint hint,float position) 
+        {
+            Hint = hint;
+            Position = position;
+        }
+        public RueIHint(string content, float duration = 3f, bool show = true, float position = 0)
+        {
+            Hint = new Hint(content,duration,show);
+            Position = position;
+
+        }
+        public RueIHint() 
+        { 
+            Hint = new Hint();
+            Position = 0;
+        }
+    }
+}
