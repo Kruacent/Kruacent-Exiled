9132ac98e89b9656adb6ee4f0de0b2927fdd92d6
Patrique29 <baka.coca@gmail.com>
2025-03-26 05:03:09 +0100

    quality

---
+++ KruacentExiled/KE.Utils/Quality/AdminToyQuality.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features.Toys;
+using KE.Utils.Quality.Enums;
+using System.Collections.Generic;
+
+namespace KE.Utils.Quality
+{
+    public static class QualityToysHandler 
+    {
+        private static Dictionary<Primitive, bool> _pickupPrimitives = new();
+        private static Dictionary<Primitive,ModelQuality> _qualityPrimitives= new();
+
+        public static bool IsPickupPrimitive(Primitive primitive)
+        {
+            return _pickupPrimitives.ContainsKey(primitive);
+        }
+
+        public static bool IsQualityPrimitive(Primitive primitive) 
+        { 
+            return _qualityPrimitives.ContainsKey(primitive); 
+        }
+
+
+        
+
+        
+    }
+}
---
+++ KruacentExiled/KE.Utils/Quality/Settings/QualitySettings.cs
@@ -0,0 +1,53 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Core.UserSettings;
+using Exiled.Events.Commands.Hub;
+using KE.Utils.Quality.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserSettings.ServerSpecific;
+
+namespace KE.Utils.Quality.Settings
+{
+    internal class QualitySettings
+    {
+
+        private static int _idQuality = 0;
+        private static int _idModels = 1;
+        public QualitySettings()
+        {
+            HeaderSetting header = new("Quality Settings");
+            SettingBase[] settings = new SettingBase[]
+            {
+                header,
+                new TwoButtonsSetting(_idQuality,"ModelQuality","Low","High"),
+                new TwoButtonsSetting(_idModels,"Pickup models","off","on")
+            };
+            SettingBase.Register(settings);
+            SettingBase.SendToAll();
+
+            
+
+        }
+
+        public static ModelQuality Get(Player p)
+        {
+            if (!SettingBase.TryGetSetting<TwoButtonsSetting>(p, _idQuality, out var setting)) return ModelQuality.None;
+            if (setting.IsSecond)
+                return ModelQuality.High;
+            else
+                return ModelQuality.Low;
+        }
+
+
+        public static bool PickmodelActivated(Player p)
+        {
+            if (!SettingBase.TryGetSetting<TwoButtonsSetting>(p, _idModels, out var setting)) return false;
+            return setting.IsSecond;
+        }
+
+
+    }
+}
