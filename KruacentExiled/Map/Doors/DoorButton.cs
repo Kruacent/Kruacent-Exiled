7b062d3db6d59a79a5ab461809d1b1338d733cdc
Patrique29 <baka.coca@gmail.com>
2025-06-04 03:23:18 +0200

    door almost done

--- KruacentExiled/Map/Doors/DoorButton.cs
+++ KruacentExiled/Map/Doors/DoorButton.cs
@@ -2,6 +2,7 @@
 using Exiled.API.Features.Toys;
 using Exiled.API.Interfaces;
 using Exiled.Events.EventArgs.Player;
+using KE.Map.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,20 +17,50 @@ namespace KE.Map.Doors
         public Vector3 Position { get; }
         public Quaternion Rotation { get; }
 
-        private InteractiblePickup _ipickup;
+        public bool IsOpen
+        {
+            get
+            {
+                return isOpen;
+            }
+            set
+            {
+                isOpen = value;
+                UpdateColor();
+            }
+        }
+        private bool isOpen;
+
+        internal InteractiblePickup _ipickup { get; }
+        private Primitive _primitive;
 
         internal DoorButton(Vector3 position,Quaternion rotation)
         {
             Position = position;
             Rotation = rotation;
-            _ipickup = new(ItemType.Medkit, Position,Vector3.one, Rotation,false);
+            _ipickup = new(ItemType.Medkit, Position, Vector3.one,0, Rotation, false);
 
-            Primitive pr = Primitive.Create(PrimitiveType.Cube, Position, null, _ipickup.GetPickupTrueSize() + new Vector3(.1f, .1f, .1f));
-            pr.Collidable = false;
+            _primitive = Primitive.Create(PrimitiveType.Cube, Position, null, _ipickup.GetPickupTrueSize() + new Vector3(.1f, .1f, .1f));
+            _primitive.Collidable = false;
 
         }
+        public void Destroy()
+        {
+            _primitive.Destroy();
+            _ipickup.Destroy();
+        }
 
-
+        private void UpdateColor()
+        {
+            if (isOpen)
+            {
+                _primitive.Color = Color.red;
+            }
+            else
+            {
+                _primitive.Color = Color.green;
+            }
+        }
         
     }
 }
