d058b8241aefb52cb8742a3d59fee656be2b17ab
Patrique29 <baka.coca@gmail.com>
2025-06-25 19:24:16 +0200

    sercret terstsx

--- KruacentExiled/Map/EasterEggs/Capybaras.cs
+++ KruacentExiled/Map/EasterEggs/Capybaras.cs
@@ -1,4 +1,5 @@
-﻿using Exiled.API.Extensions;
+﻿using AdminToys;
+using Exiled.API.Extensions;
 using Exiled.API.Features.Toys;
 using Exiled.API.Interfaces;
 using KE.Utils.API.Interfaces;
@@ -35,6 +36,10 @@ namespace KE.Map.EasterEggs
         {
             //e
             _spinnyBaras.Add(new SpinnyBaras(_capy1));
+
+
+            var l = LabApi.Features.Wrappers.TextToy.Create(_capy1);
+            l.TextFormat = "Celui qui trouve tous les easters eggs gagne 5€ Paypal";
         }
     }
 }
