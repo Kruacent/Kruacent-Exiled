64549313568050f8ab4c0fcfc7aa11bf2a71cf2e
Patrique29 <baka.coca@gmail.com>
2025-10-15 20:32:38 +0200

    inverted now rarer and healable

--- KruacentExiled/KE.CustomRoles/CR/Human/Inverted.cs
+++ KruacentExiled/KE.CustomRoles/CR/Human/Inverted.cs
@@ -1,11 +1,12 @@
-﻿using Exiled.API.Features.Attributes;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Attributes;
 using Exiled.CustomRoles.API.Features;
+using Exiled.Events.EventArgs.Player;
 using KE.CustomRoles.API.Features;
 using MEC;
 using PlayerRoles;
-using Exiled.API.Features;
 using UnityEngine;
-using Exiled.API.Enums;
 
 namespace KE.CustomRoles.CR.Human
 {
@@ -15,10 +16,10 @@ namespace KE.CustomRoles.CR.Human
         public override string Description { get; set; } = "Tu as un talent assez exceptionnel !";
         public override uint Id { get; set; } = 1066;
         public override string PublicName { get; set; } = "Inverted";
-        public override bool KeepRoleOnDeath { get; set; } = true;
-        public override bool KeepRoleOnChangingRole { get; set; } = true;
+        public override bool KeepRoleOnDeath { get; set; } = false;
+        public override bool KeepRoleOnChangingRole { get; set; } = false;
 
-        public override float SpawnChance { get; set; } = 100;
+        public override float SpawnChance { get; set; } = 20;
         public override Vector3 Scale { get; set; } = new Vector3(1, 1, 1);
 
         protected override void RoleAdded(Player player)
@@ -31,6 +32,27 @@ namespace KE.CustomRoles.CR.Human
         {
             player.Scale = new Vector3(1f, 1f, 1f);
             player.DisableEffect(EffectType.Slowness);
+        }
+
+
+        protected override void SubscribeEvents()
+        {
+            Exiled.Events.Handlers.Player.UsedItem += OnUsedItem;
+            base.SubscribeEvents();
+        }
+        protected override void UnsubscribeEvents()
+        {
+            Exiled.Events.Handlers.Player.UsedItem -= OnUsedItem;
+            base.UnsubscribeEvents();
+        }
+
+        private void OnUsedItem(UsedItemEventArgs ev)
+        {
+            if (!Check(ev.Player)) return;
+            if (ev.Item.Type == ItemType.SCP500)
+            {
+                RemoveRole(ev.Player);
+            }
         }
     }
 }
