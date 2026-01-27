03980ccb144a506176b42a83afe30972bc92783e
Patrique29 <baka.coca@gmail.com>
2026-01-27 21:46:25 +0100

    mdoel

---
+++ KruacentExiled/KE.Items/Items/PickupModels/HolyGrenadePModel.cs
@@ -0,0 +1,20 @@
+﻿using KE.Items.API.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KE.Items.Items.PickupModels
+{
+    public class HolyGrenade : PickupModel
+    {
+        public override float Scale => 0.1f;
+
+        protected override void CreateModel(Transform parent)
+        {
+
+        }
+    }
+}
