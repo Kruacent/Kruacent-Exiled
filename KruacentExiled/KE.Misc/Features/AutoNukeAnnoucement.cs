c7026900a4f5359d7ea6a1d2bba073910e33496d
Patrique29 <baka.coca@gmail.com>
2026-02-09 19:08:28 +0100

    remove killing nuek

--- KruacentExiled/KE.Misc/Features/AutoNukeAnnoucement.cs
+++ KruacentExiled/KE.Misc/Features/AutoNukeAnnoucement.cs
@@ -15,33 +15,16 @@ namespace KE.Misc.Features
     {
         public void SubscribeEvents()
         {
-            Exiled.Events.Handlers.Warhead.Detonated += OnDetonated;
         }
 
         public void UnsubscribeEvents()
         {
-            Exiled.Events.Handlers.Warhead.Detonated -= OnDetonated;
             
         }
 
         private void OnDetonated()
         {
-            Timing.CallDelayed(10, () =>
-            {
-                foreach(Player player in Player.Enumerable)
-                {
-                    if(player.Zone != Exiled.API.Enums.ZoneType.Surface)
-                    {
-                        player.Kill(Exiled.API.Enums.DamageType.Warhead);
-                    }
 
-                    if (player.Lift is not null)
-                    {
-                        player.Kill(Exiled.API.Enums.DamageType.Warhead);
-                    }
-
-                }
-            });
         }
 
 
