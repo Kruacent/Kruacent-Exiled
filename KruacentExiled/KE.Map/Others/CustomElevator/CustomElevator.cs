e10324ca0e07f542b478df2e71b87cfcdb0e93e6
Patrique29 <baka.coca@gmail.com>
2026-02-24 16:29:24 +0100

    elevator wrapper

---
+++ KruacentExiled/KE.Map/Others/CustomElevator/CustomElevator.cs
@@ -0,0 +1,29 @@
+﻿using KE.Map.Surface.ElevatorGateA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KE.Map.Others.CustomElevator
+{
+    public class CustomElevator : KEElevator
+    {
+
+        public static ElevatorModel model { get; } = new();
+
+        public override bool IsReady => false;
+
+        public override void Send()
+        {
+
+        }
+
+
+        public CustomElevator()
+        {
+            
+        }
+    }
+}
---
+++ KruacentExiled/KE.Map/Others/CustomElevator/KEElevator.cs
@@ -0,0 +1,29 @@
+﻿using CustomPlayerEffects;
+using LabApi.Features.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KE.Map.Others.CustomElevator
+{
+    public abstract class KEElevator
+    {
+
+        public static HashSet<KEElevator> elevators = new();
+
+
+
+        public abstract bool IsReady { get; }
+        public abstract void Send();
+
+
+
+        public KEElevator()
+        {
+            elevators.Add(this);
+        }
+
+    }
+}
