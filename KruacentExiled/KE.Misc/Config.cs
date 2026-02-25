993ec7f8290398da8227e51d384c66ff13c241bb
Patrique29 <baka.coca@gmail.com>
2026-02-25 17:25:27 +0100

    patch ntoe

--- KruacentExiled/KE.Misc/Config.cs
+++ KruacentExiled/KE.Misc/Config.cs
@@ -29,7 +29,7 @@ namespace KE.Misc
         public int MinPlayerVote { get; set; } = 6;
 
 
-        public string PatchNote { get; set; } = "";
+        public string PatchNote { get; set; } = "-Ajout d'un truc à Surface\r\n-Patch note dans le lobby\r\n-Ajout d'un nouveau custom SCP :\r\nun 049 qui n'a pas de zombie mais des buff à la place\r\n-Ajout d'un bouton pour remontré la description d'un custom role\r\n-Ajout d'un nouveau custom role : Le pacifiste (ClassD et Scientifique)\r\n-Ajout d'un nouveau custom role pour SCP173\r\n-Ajout d'une lumière au dos du Terroriste\r\n-Ajout d'une indication pour SetPosition\r\nuniquement visible pour le joueur ayant utilisé l'ability\r\n-Mise à jour des pickups models de la TP Grenada et de la Mine\r\n-La nuke (ni deadman ni auto) ajoute un respawn à la faction qui l'a activé\r\net met le timer à ~45s du respawn\r\n-auto nuke : 30 min -> 20 min\r\n-SCP-173 gambling est de nouveau interactable\r\nmais les items ne drop plus (sauf par manque de place)\r\nle temps d'utilisation : 10s -> 5s (approx)\r\n-Ajout de traduction aux custom roles et aux abilités\r\n-Shield Belt: 110HP -> 210HP\r\nAjout d'une taille min à la sphere de protection\r\n-Molotov <=> Heal zone à 914\r\n-Enlevé SCP-202\r\n-Removed Sou Hiyori from the facility\r\n";
 
 
 
