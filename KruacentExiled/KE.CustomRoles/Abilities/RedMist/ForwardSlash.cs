b9db633047b72fd413f1f321a3339a20a0325d0e
Patrique29 <baka.coca@gmail.com>
2026-03-01 14:55:59 +0100

    fwr slash

--- KruacentExiled/KE.CustomRoles/Abilities/RedMist/ForwardSlash.cs
+++ KruacentExiled/KE.CustomRoles/Abilities/RedMist/ForwardSlash.cs
@@ -1,13 +1,20 @@
-﻿using Exiled.API.Features;
-using KE.CustomRoles.API.Features;
-using System.Collections.Generic;
-using UnityEngine;
-using PlayerRoles.FirstPersonControl.Thirdperson;
-using PlayerRoles;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.DamageHandlers;
+using Exiled.API.Features.Items;
 using Exiled.API.Features.Roles;
 using Exiled.API.Features.Toys;
-using Exiled.API.Enums;
+using Exiled.API.Interfaces;
+using Exiled.CreditTags.Features;
+using InventorySystem.Items.MicroHID.Modules;
+using KE.CustomRoles.API.Features;
 using KE.CustomRoles.CR.MTF.RedMist;
+using PlayerRoles;
+using PlayerRoles.FirstPersonControl.Thirdperson;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
 namespace KE.CustomRoles.Abilities.RedMist
 {
     public class ForwardSlash : KEAbilities
@@ -22,7 +29,7 @@ namespace KE.CustomRoles.Abilities.RedMist
                     [TranslationKeyName] = "Forward Slash",
                     [TranslationKeyDesc] = "Manifest your E.G.O, gain powerful buff but rapid health drain.\nCan be deactivated anytime",
                     ["ForwardSlashFailEGO"] = "You need to manifest your E.G.O. first",
-                    ["ForwardSlashFailWeapon"] = "You need to manifest your E.G.O. first",
+                    ["ForwardSlashFailWeapon"] = "You need your weapon",
                 },
                 ["fr"] = new()
                 {
@@ -32,10 +39,11 @@ namespace KE.CustomRoles.Abilities.RedMist
             };
         }
         public override float Cooldown { get; } = 0f;
-
+        public const float Damage = 100;
 
         public const float MaxDistance = 15;
 
+        private RaycastHit[] NonAlloc = new RaycastHit[16];
         protected override bool AbilityUsed(Player player)
         {
 
@@ -62,13 +70,32 @@ namespace KE.CustomRoles.Abilities.RedMist
 
             Vector3 forward = player.Transform.forward;
 
-            if(Physics.Raycast(player.Position, forward,out RaycastHit hit, MaxDistance, (int)~LayerMasks.Hitbox))
+            float size = 2;
+
+            Vector3 position = player.Position;
+
+            int detect = Physics.SphereCastNonAlloc(position, size, player.CameraTransform.forward, NonAlloc, MaxDistance, HitregUtils.DetectionMask);
+
+            Draw.Sphere(position, Quaternion.identity, Vector3.one * size * 2f, Color.green, 10, Player.Enumerable);
+            Vector3 direction = player.CameraTransform.forward;
+            direction.Normalize();
+            Vector3 end = position + direction * MaxDistance;
+            Draw.Sphere(end, Quaternion.identity, Vector3.one * size * 2f, Color.yellow, 10,Player.Enumerable);
+
+            for (int i = 0;i < detect;i++)
             {
-                if(Physics.Linecast(player.Position, hit.point,out RaycastHit hitPlayer,(int)LayerMasks.Hitbox))
+                Collider collider = NonAlloc[i].collider;
+
+                if (collider.TryGetComponent<IDestructible>(out var destructible) &&
+                    (!Physics.Linecast(position, destructible.CenterOfMass, out var hitInfo, PlayerRolesUtils.AttackMask)
+                    || collider == hitInfo.collider))
                 {
-                    
+                    Player target = Player.Get(collider);
+                    destructible.Damage(Damage, new CustomDamageHandler(target, player, Damage, DamageType.Scp1509), destructible.CenterOfMass);
                 }
+
             }
+            
 
 
 
