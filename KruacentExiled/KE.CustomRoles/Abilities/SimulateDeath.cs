66619e78fe9ef981719b4ed0b6445d19c47276f4
OmerGS <27omerf@gmail.com>
2026-03-04 13:11:16 +0100

    feat: block ability for a specific player during a specific time

--- KruacentExiled/KE.CustomRoles/Abilities/SimulateDeath.cs
+++ KruacentExiled/KE.CustomRoles/Abilities/SimulateDeath.cs
@@ -3,6 +3,7 @@ using Exiled.API.Extensions;
 using Exiled.API.Features;
 using KE.CustomRoles.API.Features;
 using KE.CustomRoles.API.Interfaces;
+using KE.CustomRoles.API.Interfaces.Ability;
 using MEC;
 using PlayerRoles.FirstPersonControl.Thirdperson.Subcontrollers;
 using PlayerStatsSystem;
@@ -11,7 +12,7 @@ using UnityEngine;
 
 namespace KE.CustomRoles.Abilities
 {
-    public class SimulateDeath : KEAbilities, ICustomIcon
+    public class SimulateDeath : KEAbilities, ICustomIcon, IDuration
     {
         public override string Name { get; } = "SimulateDeath";
 
@@ -32,30 +33,35 @@ namespace KE.CustomRoles.Abilities
             };
         }
 
-        public int Duration = 10;
         public Utils.API.GifAnimator.TextImage IconName => MainPlugin.Instance.icons[Name];
         public override float Cooldown { get; } = 60f;
+        public float Duration { get; set; } = 10f;
+
+        private Ragdoll ragdoll;
+        private Vector3 pScale;
+        private Vector3 pPos;
 
         protected override bool AbilityUsed(Player player)
         {
             Dictionary<byte, DeathTranslation> deathTranslation = DeathTranslations.TranslationsById;
 
-            Vector3 pScale = player.Scale;
-            Vector3 pPos = player.Position;
-            Ragdoll ragdoll = Ragdoll.CreateAndSpawn(player.Role, player.DisplayNickname, deathTranslation[(byte)UnityEngine.Random.Range(0, deathTranslation.Count)].DeathscreenTranslation, player.Position, player.ReferenceHub.PlayerCameraReference.rotation, player);
+            this.pScale = player.Scale;
+            this.pPos = player.Position;
+            this.ragdoll = Ragdoll.CreateAndSpawn(player.Role, player.DisplayNickname, deathTranslation[(byte)UnityEngine.Random.Range(0, deathTranslation.Count)].DeathscreenTranslation, player.Position, player.ReferenceHub.PlayerCameraReference.rotation, player);
             
             player.EnableEffect(EffectType.Invisible, this.Duration);
             player.EnableEffect(EffectType.Ensnared, this.Duration);
             player.EnableEffect(EffectType.AmnesiaItems, this.Duration);
             player.Scale = new Vector3(0.1f, 0.1f, 0.1f);
 
-            Timing.CallDelayed(this.Duration, () =>
-            {
-                ragdoll.Destroy();
-                player.Scale = pScale;
-                player.Position = pPos;
-            });
             return base.AbilityUsed(player);
+        }
+
+        public void ActionAfterAbility(Player player)
+        {
+            this.ragdoll.Destroy();
+            player.Scale = this.pScale;
+            player.Position = this.pPos;
         }
     }
 }
