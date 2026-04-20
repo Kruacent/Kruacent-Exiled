7e79ca1d955102660ed5252bf6b04e1986f9d45f
Patrique29 <baka.coca@gmail.com>
2026-04-20 06:04:08 +0200

    added an add kill command

---
+++ KruacentExiled/CustomRoles/Commands/KECR/Remove.cs
@@ -0,0 +1,32 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KruacentExiled.CustomRoles.Commands.KECR
+{
+    public class Remove : ICommand
+    {
+        public string Command => "remove";
+
+        public string[] Aliases => new string[] { "r" };
+
+        public string Description => "remove a custom role";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+
+            if (!Player.TryGet(sender, out Player player))
+            {
+                response = "not a valid player";
+                return false;
+            }
+
+
+
+        }
+    }
+}
