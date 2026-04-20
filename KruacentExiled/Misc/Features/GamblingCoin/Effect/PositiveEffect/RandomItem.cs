18af6ece17805eec79812fcd1d08ade29c5b514c
Patrique29 <baka.coca@gmail.com>
2026-04-20 13:59:24 +0200

    fix coin crasj

--- KruacentExiled/Misc/Features/GamblingCoin/Effect/PositiveEffect/RandomItem.cs
+++ KruacentExiled/Misc/Features/GamblingCoin/Effect/PositiveEffect/RandomItem.cs
@@ -3,6 +3,7 @@ using Exiled.API.Features.Items;
 using KruacentExiled.Misc.Features.GamblingCoin.Interfaces;
 using KruacentExiled.Misc.Features.GamblingCoin.Types;
 using System;
+using System.Collections.Generic;
 
 internal class RandomItem : ICoinEffect
 {
@@ -11,11 +12,26 @@ internal class RandomItem : ICoinEffect
     public int Weight { get; set; } = 35;
     public EffectType Type { get; set; } = EffectType.Positive;
 
+    private HashSet<ItemType> Blacklist = new HashSet<ItemType>()
+    {
+        ItemType.None,
+    };
+
     public void Execute(Player player)
     {
+        if (player == null) return;
         Array values = Enum.GetValues(typeof(ItemType));
         ItemType randomItem = (ItemType)values.GetValue(UnityEngine.Random.Range(0, values.Length));
 
-        Item.Create(randomItem).CreatePickup(player.Position);
+        if (Blacklist.Contains(randomItem))
+        {
+            return;
+        }
+
+        Item item = Item.Create(randomItem);
+
+        if (item == null) return;
+
+        item.CreatePickup(player.Position);
     }
 }
