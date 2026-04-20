d1f076c164fcdb429689926809764fc812a9e1f8
Patrique29 <baka.coca@gmail.com>
2026-04-20 07:17:45 +0200

    nerf ultra 939

--- KruacentExiled/CustomRoles/CR/SCP/SCP939/Ultra.cs
+++ KruacentExiled/CustomRoles/CR/SCP/SCP939/Ultra.cs
@@ -1,11 +1,15 @@
 ﻿using Exiled.API.Enums;
 using Exiled.API.Features;
 using HintServiceMeow.Core.Models.Arguments;
+using HintServiceMeow.Core.Utilities;
 using KE.Utils.API.Displays.DisplayMeow;
 using KE.Utils.API.Displays.DisplayMeow.Placements;
+using KE.Utils.API.Features;
 using KruacentExiled.CustomRoles.API.Core.Positions;
 using KruacentExiled.CustomRoles.API.Features;
+using KruacentExiled.CustomRoles.CustomSCPTeam;
 using MEC;
+using Mirror;
 using PlayerRoles;
 using System;
 using System.Collections.Generic;
@@ -47,17 +51,67 @@ namespace KruacentExiled.CustomRoles.CR.SCP.SCP939
         public const int SizeText = 20;
 
         public static readonly HintPosition UltraPosition = new UltraPosition();
+
+
+        public const float UpdateCooldown = 60f;
+        private Dictionary<Player, DateTime> lastUpdate;
+        private Dictionary<Player, CoroutineHandle> handles;
+
+        protected override void SubscribeEvents()
+        {
+            lastUpdate = new Dictionary<Player, DateTime>();
+            handles = new Dictionary<Player, CoroutineHandle>();
+            base.SubscribeEvents();
+        }
+
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+            lastUpdate = null;
+            handles = null;
+        }
+
+
+
         protected override void RoleAdded(Player player)
         {
-            Timing.CallDelayed(1f, () =>
-            {
-                DisplayHandler.Instance.CreateAuto(player, (arg) => PlayerInZone(arg), UltraPosition.HintPlacement);
-            });
+            lastUpdate.Add(player, DateTime.Now);
+            //DisplayHandler.Instance.CreateAuto(player, (arg) => PlayerInZone(player), UltraPosition.HintPlacement, HintServiceMeow.Core.Enum.HintSyncSpeed.UnSync);
+            handles.Add(player, Timing.RunCoroutine(Loop(player)));
+
         }
-        private string PlayerInZone(AutoContentUpdateArg arg)
+
+
+        protected override void RoleRemoved(Player player)
+        {
+            DisplayHandler.Instance.RemoveHint(player, UltraPosition.HintPlacement);
+            lastUpdate.Remove(player);
+
+            Timing.KillCoroutines(handles[player]);
+            handles.Remove(player);
+        }
+
+        private IEnumerator<float> Loop(Player player)
+        {
+            while (Check(player))
+            {
+                PlayerDisplay display = PlayerDisplay.Get(player);
+                KELog.Debug("update");
+                DisplayHandler.Instance.AddHint(UltraPosition.HintPlacement, player, PlayerInZone(player), UpdateCooldown);
+                yield return Timing.WaitForSeconds(UpdateCooldown);
+                
+
+            }
+
+
+        }
+
+
+
+        private string PlayerInZone(Player player)
         {
 
-            if (!TrackedPlayers.Contains(Player.Get(arg.PlayerDisplay.ReferenceHub)))
+            if (!Check(player))
             {
                 return string.Empty;
             }
@@ -78,7 +132,7 @@ namespace KruacentExiled.CustomRoles.CR.SCP.SCP939
 
         private int GetPlayerInZone(ZoneType zone)
         {
-            return Player.List.Count(p => !p.IsScp && p.Zone == zone);
+            return Player.Enumerable.Count(p => !SCPTeam.IsSCP(p.ReferenceHub) && p.IsAlive && p.Zone == zone);
         }
     }
 }
